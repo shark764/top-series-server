99c1c4d2eb598be379ac4911961c7180625e838f
Farid Hernandez <farid.hdz.64@gmail.com>
2021-01-19 19:23:51 -0600

    Fixing Serie query type on Actor type

--- src/resolvers/Actor.js
+++ src/resolvers/Actor.js
@@ -1,8 +1,6 @@
 const resolver = {
-  series: async (parent, args, context) => {
-    return context.prisma.actor
-      .findUnique({ where: { id: parent.id } })
-      .series();
+  cast: async (parent, args, context) => {
+    return context.prisma.actor.findUnique({ where: { id: parent.id } }).cast();
   },
 };
 
